a4564c3af10e250c4a3394bc04ae61009f5fab3d
babsyinks <babawarunn@yahoo.com>
2021-02-10 19:36:40 +0100

    added connection string for mongodb atlas

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') 
 mongoose.set('useCreateIndex', true)
-const conn = mongoose.connect('mongodb://localhost:27017/bettingSiteDB',{useNewUrlParser: true, useUnifiedTopology: true})
+const db = process.env.NODE_ENV === 'production'?process.env.MONGODB_CONN_STR_PROD:process.env.MONGODB_CONN_STR_LOCAL
+const conn = mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true})
 conn.then(()=>{
     console.log('Connected successfully to bettingSiteDB successfully')
 })
