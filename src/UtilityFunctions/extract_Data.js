f4c7490f855650059e805b3dd6291b4f44c797f8
babsyinks <babawarunn@yahoo.com>
2021-02-11 20:11:26 +0100

    reformatted image links

--- src/UtilityFunctions/extract_Data.js
+++ src/UtilityFunctions/extract_Data.js
@@ -1,8 +1,4 @@
 export default async function extractData(country,league) {
-<<<<<<< HEAD
-=======
-/*  */
->>>>>>> b2d4bb1e8256bd219cda10cc4bf3c59c3a20a835
 //push arrs of form [{name: 'main',match: match1,'1': '2.30','X': '3.40','2': '2.90','1X': '1.40','12': '1.33','X2': '1.56'},{name: 'gg_ng',match: match1,gg: '1.65',ng: '2.46' },etc] to epl_vals
 //returned arr is of form [[{},{}],[{},{}],etc]
 try {
@@ -22,7 +18,3 @@ return matchesObj
 }
   }
 
-<<<<<<< HEAD
-=======
-  //remember to return arr in above function
->>>>>>> b2d4bb1e8256bd219cda10cc4bf3c59c3a20a835
